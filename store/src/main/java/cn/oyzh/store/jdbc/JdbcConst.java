156d624180c30ba5ece3cce63ba9447cf1c39874
oyzh <oyzh.1994@qq.com>
2024-11-18 18:33:51 +0800

    1. 储存功能完善

--- store/src/main/java/cn/oyzh/store/jdbc/JdbcConst.java
+++ store/src/main/java/cn/oyzh/store/jdbc/JdbcConst.java
@@ -3,16 +3,27 @@ package cn.oyzh.store.jdbc;
 import lombok.experimental.UtilityClass;
 
 /**
+ * db常量
+ *
  * @author oyzh
  * @since 2024-09-27
  */
 @UtilityClass
 public class JdbcConst {
 
+    /**
+     * db文件
+     */
     public String DB_FILE = "db.file";
 
+    /**
+     * db方言
+     */
     public String DB_DIALECT = "db.dialect";
 
+    /**
+     * db缓存
+     */
     public String DB_CACHE_SIZE = "db.cache.size";
 
 }
