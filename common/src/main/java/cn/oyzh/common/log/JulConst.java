459194d3cef5c12d44b17e3a5341524335d38e4a
oyzh <oyzh.1994@qq.com>
2024-11-19 18:43:45 +0800

    1. 功能完善

---
+++ common/src/main/java/cn/oyzh/common/log/JulConst.java
@@ -0,0 +1,27 @@
+package cn.oyzh.common.log;
+
+import cn.oyzh.common.util.StringUtil;
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author oyzh
+ * @since 2024-11-19
+ */
+@UtilityClass
+public class JulConst {
+
+    public static final String ENABLE_THREAD_ID = "jul.enable.thread.id";
+
+    public static void enableThreadId() {
+        System.setProperty(ENABLE_THREAD_ID, "true");
+    }
+
+    public static void disableThreadId() {
+        System.clearProperty(ENABLE_THREAD_ID);
+    }
+
+    public static boolean isEnableThreadId() {
+        String prop = System.getProperty(ENABLE_THREAD_ID);
+        return StringUtil.equals(prop, "true");
+    }
+}
