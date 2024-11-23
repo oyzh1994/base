2ec06b177eccfc14d27c237a2db386e86bee1bfa
oyzh <oyzh.1994@qq.com>
2024-11-23 22:47:11 +0800

    1. 功能完善

---
+++ common/src/main/java/cn/oyzh/common/util/BitUtil.java
@@ -0,0 +1,29 @@
+package cn.oyzh.common.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.BitSet;
+
+@UtilityClass
+public class BitUtil {
+
+    public static boolean isTrue(byte b, int index) {
+        return new BitSet(b).get(index);
+    }
+
+    public static boolean isFalse(byte b, int index) {
+        return !new BitSet(b).get(index);
+    }
+
+    public static void setTrue(byte b, int index) {
+        new BitSet(b).set(index, true);
+    }
+
+    public static void setFalse(byte b, int index) {
+        new BitSet(b).set(index, false);
+    }
+
+    public static void setValue(byte b, int index, boolean value) {
+        new BitSet(b).set(index, value);
+    }
+}
