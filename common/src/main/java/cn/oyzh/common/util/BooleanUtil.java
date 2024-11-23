5460b35d17016fda3ee35ffabeb2edd51574ca81
oyzh <oyzh.1994@qq.com>
2024-11-23 22:47:11 +0800

    1. 功能完善

--- common/src/main/java/cn/oyzh/common/util/BooleanUtil.java
+++ common/src/main/java/cn/oyzh/common/util/BooleanUtil.java
@@ -1,9 +1,12 @@
 package cn.oyzh.common.util;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * @author oyzh
  * @since 2024-09-29
  */
+@UtilityClass
 public class BooleanUtil {
 
     public static boolean isTrue(Boolean bool) {
