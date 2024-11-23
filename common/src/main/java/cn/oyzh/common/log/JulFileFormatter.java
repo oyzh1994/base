9e3ca2f11e69de5a270d834c280d33701a10d5ba
oyzh <oyzh.1994@qq.com>
2024-11-23 12:26:11 +0800

    1. 日志功能完善 2. json功能完善

--- common/src/main/java/cn/oyzh/common/log/JulFileFormatter.java
+++ common/src/main/java/cn/oyzh/common/log/JulFileFormatter.java
@@ -4,61 +4,16 @@ import cn.oyzh.common.date.DateHelper;
 import cn.oyzh.common.util.ArrayUtil;
 import cn.oyzh.common.util.StringUtil;
 
-import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 /**
- * 控制台日志格式化器
+ * 日志格式化器
  *
  * @author oyzh
- * @since 2024-11-15
+ * @since 2024-09-27
  */
 
-public class JulConsoleLogFormatter extends JulFormatter {
-    /**
-     * 重置
-     */
-    public static final String ANSI_RESET = "\u001B[0m";
-
-    /**
-     * 黑色
-     */
-    public static final String ANSI_BLACK = "\u001B[30m";
-
-    /**
-     * 红色
-     */
-    public static final String ANSI_RED = "\u001B[31m";
-
-    /**
-     * 绿色
-     */
-    public static final String ANSI_GREEN = "\u001B[32m";
-
-    /**
-     * 黄色
-     */
-    public static final String ANSI_YELLOW = "\u001B[33m";
-
-    /**
-     * 蓝色
-     */
-    public static final String ANSI_BLUE = "\u001B[34m";
-
-    /**
-     * 紫色
-     */
-    public static final String ANSI_PURPLE = "\u001B[35m";
-
-    /**
-     * 青蓝
-     */
-    public static final String ANSI_CYAN = "\u001B[36m";
-
-    /**
-     * 白色
-     */
-    public static final String ANSI_WHITE = "\u001B[37m";
+public class JulFileFormatter extends JulFormatter {
 
     @Override
     public String format(LogRecord record) {
@@ -69,42 +24,25 @@ public class JulConsoleLogFormatter extends JulFormatter {
 
         // 日志等级
         JulLevel level = JulLevel.ofLevel(record.getLevel());
-        if (level == JulLevel.TRACE || level == JulLevel.DEBUG || level == JulLevel.INFO) {
-            builder.append(ANSI_GREEN);
-            builder.append(" ").append(level);
-            builder.append(ANSI_RESET);
-        } else if (level == JulLevel.WARN) {
-            builder.append(ANSI_YELLOW);
-            builder.append(" ").append(level);
-            builder.append(ANSI_RESET);
-        } else if (level == JulLevel.ERROR) {
-            builder.append(ANSI_RED);
-            builder.append(" ").append(level);
-            builder.append(ANSI_RESET);
-        }
+        builder.append(" ").append(level);
 
         // 线程id
         if (JulConst.isEnableThreadId()) {
-            builder.append(ANSI_PURPLE);
             builder.append(" ").append(record.getLongThreadID());
-            builder.append(ANSI_RESET);
         }
 
         // 线程名称
         if (record instanceof JulLogRecord logRecord) {
-            builder.append(ANSI_CYAN);
             builder.append(" [").append(logRecord.getThreadName()).append("]");
-            builder.append(ANSI_RESET);
         }
 
         // 类名、方法名、行号
-        builder.append(ANSI_BLUE);
         builder.append(" ").append(record.getSourceClassName());
         builder.append(".").append(record.getSourceMethodName());
         if (record instanceof JulLogRecord logRecord) {
             builder.append("#").append(logRecord.getLineNumber());
         }
-        builder.append(ANSI_RESET);
+
         // 消息处理
         builder.append(" : ").append(this.formatMessage(record));
         builder.append("\n");
@@ -127,7 +65,7 @@ public class JulConsoleLogFormatter extends JulFormatter {
                         continue;
                     }
                     arg = this.pretreatmentArg(arg, true);
-                    message = message.replaceFirst("\\{}", ANSI_WHITE + arg + ANSI_RESET);
+                    message = message.replaceFirst("\\{}", arg.toString());
                     index++;
                 }
             } else {// jul类型占位符
@@ -138,7 +76,7 @@ public class JulConsoleLogFormatter extends JulFormatter {
                         continue;
                     }
                     arg = this.pretreatmentArg(arg, true);
-                    message = message.replaceFirst("\\{" + index + "}", ANSI_WHITE + arg + ANSI_RESET);
+                    message = message.replaceFirst("\\{" + index + "}", arg.toString());
                     index++;
                 }
             }
@@ -175,9 +113,7 @@ public class JulConsoleLogFormatter extends JulFormatter {
             if (lineNumber != -1) {
                 String[] arr = sourceClassName.split("\\.");
                 builder.append("(");
-                builder.append(ANSI_BLUE);
                 builder.append(ArrayUtil.last(arr)).append(".java:").append(lineNumber);
-                builder.append(ANSI_RESET);
                 builder.append(")");
             }
             message = builder.toString();
