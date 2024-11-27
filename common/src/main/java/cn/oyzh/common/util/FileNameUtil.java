59524b2678fb7f8e8e07f1129984463e5fb43e9a
oyzh <oyzh.1994@qq.com>
2024-11-27 15:10:54 +0800

    1. 组件完善

--- common/src/main/java/cn/oyzh/common/util/FileNameUtil.java
+++ common/src/main/java/cn/oyzh/common/util/FileNameUtil.java
@@ -14,6 +14,96 @@ import java.io.File;
 public class FileNameUtil {
 
     /**
+     * 是否sql类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public boolean isSqlType(String fileType) {
+        return "sql".equalsIgnoreCase(fileType);
+    }
+
+    /**
+     * 是否xml类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public static boolean isXmlType(String fileType) {
+        return "xml".equalsIgnoreCase(fileType);
+    }
+
+    /**
+     * 是否csv类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public static boolean isCsvType(String fileType) {
+        return "csv".equalsIgnoreCase(fileType);
+    }
+
+    /**
+     * 是否html类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public static boolean isHtmlType(String fileType) {
+        return "html".equalsIgnoreCase(fileType);
+    }
+
+    /**
+     * 是否xls类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public static boolean isXlsType(String fileType) {
+        return "xls".equalsIgnoreCase(fileType);
+    }
+
+    /**
+     * 是否xlsx类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public static boolean isXlsxType(String fileType) {
+        return "xlsx".equalsIgnoreCase(fileType);
+    }
+
+    /**
+     * 是否excel类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public static boolean isExcelType(String fileType) {
+        return isXlsType(fileType) || isXlsxType(fileType);
+    }
+
+    /**
+     * 是否json类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public static boolean isJsonType(String fileType) {
+        return "json".equalsIgnoreCase(fileType);
+    }
+
+    /**
+     * 是否txt类型
+     *
+     * @param fileType 文件类型
+     * @return 结果
+     */
+    public static boolean isTxtType(String fileType) {
+        return "txt".equalsIgnoreCase(fileType);
+    }
+
+    /**
      * 追加路径
      *
      * @param paths 路径列表
