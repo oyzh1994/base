02aaa8e2bf4a8e4ad337e0db6d29e08d86cb06cf
oyzh <oyzh.1994@qq.com>
2024-11-28 09:18:49 +0800

    1. 存储功能完善

---
+++ store/src/main/java/cn/oyzh/store/file/FileRecord.java
@@ -0,0 +1,12 @@
+package cn.oyzh.store.file;
+
+import java.util.HashMap;
+
+/**
+ * @author oyzh
+ * @since 2024-11-27
+ */
+public class FileRecord extends HashMap<Integer, Object>{
+
+
+}
---
+++ store/src/main/java/cn/oyzh/store/file/FileWriteConfig.java
@@ -0,0 +1,36 @@
+package cn.oyzh.store.file;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author oyzh
+ * @since 2024/09/02
+ */
+@Data
+@Accessors(chain = true, fluent = true)
+public class FileWriteConfig {
+
+    /**
+     * 文本识别符号
+     */
+    private String txtIdentifier = "\"";
+
+    /**
+     * 字符集
+     */
+    private String charset = StandardCharsets.UTF_8.name();
+
+    /**
+     * 文件路径
+     */
+    private String filePath;
+
+    /**
+     * 前缀
+     */
+    private String prefix;
+
+}
