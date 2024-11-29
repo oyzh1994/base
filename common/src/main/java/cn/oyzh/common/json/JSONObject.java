4b3f442ba6d72fe8444d5e8fc8e3f72868dd79b1
oyzh <oyzh.1994@qq.com>
2024-11-29 17:52:46 +0800

    1. 组件完善

--- common/src/main/java/cn/oyzh/common/json/JSONObject.java
+++ common/src/main/java/cn/oyzh/common/json/JSONObject.java
@@ -10,6 +10,9 @@ import java.util.List;
  */
 public class JSONObject extends com.alibaba.fastjson2.JSONObject {
 
+    public JSONObject() {
+    }
+
     public JSONObject(com.alibaba.fastjson2.JSONObject object) {
         super(object);
     }
