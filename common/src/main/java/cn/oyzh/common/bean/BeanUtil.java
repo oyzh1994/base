43e505cb679c67615a812444f845fd87faec8c69
oyzh <oyzh.1994@qq.com>
2024-12-05 15:46:49 +0800

    1. 功能完善

--- common/src/main/java/cn/oyzh/common/bean/BeanUtil.java
+++ common/src/main/java/cn/oyzh/common/bean/BeanUtil.java
@@ -1,75 +1,149 @@
 package cn.oyzh.common.bean;
 
-import cn.oyzh.common.util.ReflectUtil;
+import cn.oyzh.common.exception.InvalidParamException;
+import cn.oyzh.common.exception.PropertyNotFoundException;
 import cn.oyzh.common.util.StringUtil;
 import lombok.experimental.UtilityClass;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
 
 /**
  * @author oyzh
- * @since 2024-11-18
+ * @since 2024-09-24
  */
 @UtilityClass
 public class BeanUtil {
 
-    public static Method getGetterMethod(String propertyName, Class<?> propertyType, Class<?> beanType) {
-        propertyName = StringUtil.upperFirst(propertyName);
-        if (propertyType == boolean.class) {
-            return ReflectUtil.getMethod(beanType, "is" + propertyName, true, true);
-        }
-        return ReflectUtil.getMethod(beanType, "get" + propertyName, true, true);
+    public static <T> T getValue(Object bean, String name) {
+        return getValue(bean, name, true);
     }
 
-    public static Method getSetterMethod(String propertyName, Class<?> propertyType, Class<?> beanType) {
-        propertyName = StringUtil.upperFirst(propertyName);
-        String methodName = "set" + propertyName;
-        Method[] methods = ReflectUtil.getMethods(beanType, false, true);
-        for (Method method : methods) {
-            // 检查方法名
-            if (!method.getName().equals(methodName)) {
-                continue;
-            }
-            // 检查参数
-            if (method.getParameterCount() != 1) {
-                continue;
-            }
-            // 检查修饰符
-            int modifiers = method.getModifiers();
-            if (Modifier.isNative(modifiers)
-                    || Modifier.isAbstract(modifiers)
-                    || !Modifier.isPublic(modifiers)
-            ) {
-                continue;
-            }
-            Class<?> paramType = method.getParameterTypes()[0];
-            if (paramType == propertyType) {
-                return method;
-            }
-            // 其他类型
-            if (paramType.isAssignableFrom(propertyType) || propertyType.isAssignableFrom(paramType)) {
-                return method;
-            }
-            // boolean类型
-            if (Boolean.class.isAssignableFrom(propertyType) && boolean.class == paramType) {
-                return method;
-            }
-            // 数字类型
-            if (Number.class.isAssignableFrom(propertyType)) {
-                if (Number.class.isAssignableFrom(paramType)) {
-                    return method;
-                }
-                if (Arrays.asList(byte.class, short.class, int.class, float.class, double.class, long.class).contains(paramType)) {
-                    return method;
-                }
-            }
-            // 字符类型
-            if (CharSequence.class.isAssignableFrom(propertyType) && CharSequence.class.isAssignableFrom(paramType)) {
-                return method;
-            }
+    public static <T> T getValue(Object bean, String name, boolean callSuper) {
+        if (bean == null) {
+            throw new InvalidParamException("bean");
+        }
+        if (name == null) {
+            throw new InvalidParamException("name");
+        }
+        Method method = getGetterMethod(bean.getClass(), name, callSuper);
+        if (method == null) {
+            throw new PropertyNotFoundException(name);
+        }
+        try {
+            method.setAccessible(true);
+            return (T) method.invoke(bean);
+        } catch (IllegalAccessException | InvocationTargetException ignored) {
+
         }
         return null;
     }
+
+    public static Method getGetterMethod(Class<?> clazz, String name, boolean callSuper) {
+        String property = StringUtil.upperFirst(name);
+        Method method = null;
+        try {
+            method = clazz.getMethod("get" + property);
+        } catch (NoSuchMethodException ignored) {
+
+        }
+        if (method == null) {
+            try {
+                method = clazz.getMethod("is" + property);
+            } catch (NoSuchMethodException ignored) {
+
+            }
+        }
+        if (method == null && callSuper) {
+            clazz = clazz.getSuperclass();
+            if (clazz != Object.class) {
+                method = getGetterMethod(clazz, name, true);
+            }
+        }
+        return method;
+    }
+
+    public static void setValue(Object bean, String name, Object value) {
+        setValue(bean, name, value, true);
+    }
+
+    public static void setValue(Object bean, String name, Object value, boolean callSuper) {
+        if (bean == null) {
+            throw new InvalidParamException("bean");
+        }
+        if (name == null) {
+            throw new InvalidParamException("name");
+        }
+        Method method = getSetterMethod(bean.getClass(), name, value == null ? null : value.getClass(), callSuper);
+        if (method == null) {
+            throw new PropertyNotFoundException(name);
+        }
+        try {
+            method.setAccessible(true);
+            method.invoke(bean, value);
+        } catch (IllegalAccessException | InvocationTargetException ignored) {
+
+        }
+    }
+
+    public static Method getSetterMethod(Class<?> clazz, String name, Class<?> paramClass, boolean callSuper) {
+        String property = StringUtil.upperFirst(name);
+        Method method = null;
+        String mName = "set" + property;
+        for (Method clazzMethod : clazz.getMethods()) {
+            if (!clazzMethod.getName().equals(mName)) {
+                continue;
+            }
+            Class<?>[] parameterTypes = clazzMethod.getParameterTypes();
+            if (clazzMethod.getParameterTypes().length != 1) {
+                continue;
+            }
+            if (paramClass == null) {
+                method = clazzMethod;
+                break;
+            }
+            Class<?> pClass = parameterTypes[0];
+            if (paramClass == pClass || paramClass.isAssignableFrom(pClass)) {
+                method = clazzMethod;
+                break;
+            }
+            if (pClass == int.class || paramClass == Integer.class) {
+                method = clazzMethod;
+                break;
+            }
+            if (pClass == long.class || paramClass == Long.class) {
+                method = clazzMethod;
+                break;
+            }
+            if (pClass == double.class || paramClass == Double.class) {
+                method = clazzMethod;
+                break;
+            }
+            if (pClass == float.class || paramClass == Float.class) {
+                method = clazzMethod;
+                break;
+            }
+            if (pClass == byte.class || paramClass == Byte.class) {
+                method = clazzMethod;
+                break;
+            }
+            if (pClass == short.class || paramClass == Short.class) {
+                method = clazzMethod;
+                break;
+            }
+            if (pClass == char.class || paramClass == Character.class) {
+                method = clazzMethod;
+                break;
+            }
+        }
+        if (method == null && callSuper) {
+            clazz = clazz.getSuperclass();
+            if (clazz != Object.class) {
+                method = getSetterMethod(clazz, name, paramClass, true);
+            }
+        }
+        return method;
+    }
+
+
 }
