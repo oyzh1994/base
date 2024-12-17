4f90d1f1f9f78d52f6d571e7c645407ad5aeb01b
oyzh <oyzh.1994@qq.com>
2024-12-17 09:14:40 +0800

    1. 功能完善

--- common/src/main/java/cn/oyzh/common/util/BooleanUtil.java
+++ common/src/main/java/cn/oyzh/common/util/BooleanUtil.java
@@ -12,4 +12,8 @@ public class BooleanUtil {
     public static boolean isTrue(Boolean bool) {
         return bool != null && bool;
     }
+
+    public static boolean isFalse(Boolean bool) {
+        return bool != null && !bool;
+    }
 }
