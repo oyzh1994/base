a888b2c86f5eebec4c09a0888c209a456742fd6d
oyzh <oyzh.1994@qq.com>
2025-01-01 16:57:18 +0800

    1. 新增耗时统计功能

---
+++ common/src/main/java/cn/oyzh/common/util/CostUtil.java
@@ -0,0 +1,37 @@
+package cn.oyzh.common.util;
+
+
+import cn.oyzh.common.log.JulLog;
+import lombok.experimental.UtilityClass;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @since 2025/01/01
+ */
+@UtilityClass
+public class CostUtil {
+
+    private static final Map<String, Long> COST_RECORD = new ConcurrentHashMap<>();
+
+    public static void record() {
+        StackTraceElement element = Thread.currentThread().getStackTrace()[2];
+        String name = element.getFileName() + "." + element.getMethodName();
+        COST_RECORD.put(name, System.currentTimeMillis());
+    }
+
+    public static void printCost() {
+        StackTraceElement element = Thread.currentThread().getStackTrace()[2];
+        String name = element.getFileName() + "." + element.getMethodName();
+        Long start = COST_RECORD.get(name);
+        if (start == null) {
+            return;
+        }
+        String fileName = name + "#" + element.getLineNumber();
+        long end = System.currentTimeMillis();
+        JulLog.info("{}={}ms", name, end - start);
+        COST_RECORD.remove(name);
+    }
+}
