fc5c4979d5cf03995170486e3d7c2d92fc8f7636
oyzh <oyzh.1994@q.qcom>
2025-03-12 17:07:15 +0800

    1. 细节完善

--- common/src/main/java/cn/oyzh/common/exception/ExceptionUtil.java
+++ common/src/main/java/cn/oyzh/common/exception/ExceptionUtil.java
@@ -1,5 +1,6 @@
 package cn.oyzh.common.exception;
 
+import cn.oyzh.common.util.StringUtil;
 import lombok.experimental.UtilityClass;
 
 /**
@@ -11,5 +12,12 @@ public class ExceptionUtil {
 
     public static void throwRuntime(String message) throws RuntimeException {
         throw new RuntimeException(message);
+    }
+
+    public static boolean hasMessage(Exception ex, String message) throws RuntimeException {
+        if (ex == null) {
+            return false;
+        }
+        return StringUtil.containsAny(ex.getMessage(), message);
     }
 }
