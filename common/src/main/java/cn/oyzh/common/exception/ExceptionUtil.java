071a07f7a69d7a930d2f98b3b0fbcb4939e62ebf
oyzh <oyzh.1994@q.qcom>
2025-03-21 18:41:40 +0800

    1. 功能完善

--- common/src/main/java/cn/oyzh/common/exception/ExceptionUtil.java
+++ common/src/main/java/cn/oyzh/common/exception/ExceptionUtil.java
@@ -13,10 +13,22 @@ public class ExceptionUtil {
         throw new RuntimeException(message);
     }
 
-    public static boolean hasMessage(Throwable ex, String... message) throws RuntimeException {
+    public static String getMessage(Throwable ex) throws RuntimeException {
         if (ex == null) {
-            return false;
+            return null;
         }
-        return StringUtil.containsAny(ex.getMessage(), message);
+        Throwable t = ex;
+        while (t.getCause() != null) {
+            t = t.getCause();
+        }
+        String msg = t.getMessage();
+        if (msg == null || msg.isBlank()) {
+            return t.toString();
+        }
+        return msg;
+    }
+
+    public static boolean hasMessage(Throwable ex, String... message) throws RuntimeException {
+        return StringUtil.containsAny(getMessage(ex), message);
     }
 }
