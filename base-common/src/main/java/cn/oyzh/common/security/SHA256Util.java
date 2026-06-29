8c14e286139e6d9b9dd13ed3d83582dc44e39a9b
oyzh <oyzh.1994@q.qcom>
2026-06-29 12:02:08 +0800

    add: SHA256Util optimize: 注释TextUtil的hex相关代码，改用HexUtil

---
+++ base-common/src/main/java/cn/oyzh/common/security/SHA256Util.java
@@ -0,0 +1,44 @@
+package cn.oyzh.common.security;
+
+import cn.oyzh.common.util.HexUtil;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+/**
+ *
+ * @author oyzh
+ * @since 2026-06-29
+ */
+public class SHA256Util {
+
+    /**
+     * SHA256 哈希，返回十六进制字符串
+     *
+     * @param msg 消息
+     * @return 结果
+     * @throws Exception 异常
+     */
+    public static String sha256Hex(String msg) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] digest = md.digest(msg.getBytes(StandardCharsets.UTF_8));
+        return HexUtil.bytesToHex(digest);
+    }
+
+    /**
+     * HMAC-SHA256 加密，返回字节数组
+     *
+     * @param key 密钥
+     * @param msg 消息
+     * @return 结果
+     * @throws Exception 异常
+     */
+    public static byte[] hmacSha256(byte[] key, String msg) throws Exception {
+        Mac mac = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "HmacSHA256");
+        mac.init(secretKeySpec);
+        return mac.doFinal(msg.getBytes(StandardCharsets.UTF_8));
+    }
+}
--- base-common/src/main/java/cn/oyzh/common/util/HexUtil.java
+++ base-common/src/main/java/cn/oyzh/common/util/HexUtil.java
@@ -11,10 +11,23 @@ public class HexUtil {
     private HexUtil() {
     }
 
+    /**
+     * 将字节数组转换为十六进制字符串
+     *
+     * @param bytes 字节数组
+     * @return 十六进制字符串
+     */
     public static String bytesToHex(byte[] bytes) {
         return bytesToHex(bytes, true);
     }
 
+    /**
+     * 将字节数组转换为十六进制字符串
+     *
+     * @param bytes       字节数组
+     * @param toUpperCase 大写形式
+     * @return 十六进制字符串
+     */
     public static String bytesToHex(byte[] bytes, boolean toUpperCase) {
         if (bytes == null || bytes.length == 0) {
             return "";
@@ -50,4 +63,21 @@ public class HexUtil {
         }
         return bytesToHex(bytes, true);
     }
+
+    /**
+     * 将十六进制转换为字符串字节数组
+     *
+     * @param s 十六进制字符串
+     * @return 字节数组
+     */
+    public static byte[] decodeHexStr(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
 }
