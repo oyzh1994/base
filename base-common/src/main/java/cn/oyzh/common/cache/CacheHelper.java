4b619b2b8223f68f3c3d20231c5ca4f73066125d
oyzh <oyzh.1994@q.qcom>
2026-07-09 16:03:55 +0800

    update: 缓存辅助改造，须包含前缀

--- base-common/src/main/java/cn/oyzh/common/cache/CacheHelper.java
+++ base-common/src/main/java/cn/oyzh/common/cache/CacheHelper.java
@@ -1,5 +1,10 @@
 package cn.oyzh.common.cache;
 
+import cn.oyzh.common.exception.InvalidParamException;
+import cn.oyzh.common.util.StringUtil;
+
+import java.lang.ref.WeakReference;
+
 /**
  * 缓存辅助类
  *
@@ -11,18 +16,28 @@ public class CacheHelper {
     private static final TimedCache<String, Object> CACHE = CacheUtil.newTimedCache(-1);
 
     public static <T> T get(String key) {
-        return (T) CACHE.get(key);
+        if (StringUtil.notContains(key, ":")) {
+            throw new InvalidParamException(key);
+        }
+        WeakReference<?> reference = (WeakReference<?>) CACHE.get(key);
+        return reference == null ? null : (T) reference.get();
     }
 
     public static void remove(String key) {
+        if (StringUtil.notContains(key, ":")) {
+            throw new InvalidParamException(key);
+        }
         CACHE.remove(key);
     }
 
     public static void set(String key, Object val) {
-        CACHE.put(key, val);
+        if (StringUtil.notContains(key, ":")) {
+            throw new InvalidParamException(key);
+        }
+        CACHE.put(key, new WeakReference<>(val));
     }
 
-    public static void clear() {
-        CACHE.clear();
-    }
+    //    public static void clear() {
+    //        CACHE.clear();
+    //    }
 }
