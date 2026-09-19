4a9d295ae57e651fe26e31afbfec9ccb948858d7
oyzh <oyzh.1994@q.qcom>
2026-09-19 23:15:23 +0800

    update: win arm打包完善

--- base-pkg/package/pack1.bat
+++ base-pkg/package/pack1.bat
@@ -2,4 +2,4 @@
 chcp 65001 >nul
 set ARG1=%1
 call mvn package -DskipTests
-call mvn exec:java -Dexec.mainClass="cn.oyzh.pkg.test.WinArmPreHandler" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
+call mvn exec:java -Dexec.mainClass="cn.oyzh.pkg.test.WoaPreHandler" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
--- base-pkg/package/pack2.bat
+++ base-pkg/package/pack2.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001 >nul
 set ARG1=%1
-call mvn exec:java -Dexec.mainClass="cn.oyzh.pkg.test.WinArmPreHandler" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
+call mvn exec:java -Dexec.mainClass="cn.oyzh.pkg.test.WoaPreHandler" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
